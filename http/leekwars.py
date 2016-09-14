07b2b6436fd4849bc5d3d6c75fb00f8a9d8fcf84
Pilow <pierre.laupretre@gmail.com>
2016-09-14 21:28:35 +0200

    Open the browser directly

--- http/leekwars.py
+++ http/leekwars.py
@@ -1,23 +1,23 @@
 import SimpleHTTPServer, SocketServer
-import urlparse, os, re, time
+import urlparse, os, re, time, webbrowser
 
 PORT = 8012
 
 class LWHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
-		
+
 		if self.path != '/' and os.access('.' + self.path, os.R_OK):
 			SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self);
 		else:
-		
+
 			bindings = {
 				'static': 'http://localhost:' + str(PORT) + '/',
 				'time': str(int(time.time()))
 			}
-	
+
 			body = open("view/head.html").read()
 			body = re.sub(r"\{\{(.*)\}\}", lambda m: bindings[m.group(1)], body)
-			
+
 			self.send_response(200)
 			self.send_header('Content-Type', "text/html; charset=utf-8")
 			self.send_header("Content-Length", str(len(body)))
@@ -29,4 +29,5 @@ class LWTCPServer(SocketServer.TCPServer):
     allow_reuse_address = True
 server = LWTCPServer(("", PORT), LWHandler)
 print "Serving Leek Wars at port", PORT
+webbrowser.open('http://localhost:' + str(PORT))
 server.serve_forever()
