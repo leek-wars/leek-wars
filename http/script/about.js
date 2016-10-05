8443145a89415f0dddfd3cbd74936bdae0fbe448
Pilow <pierre.laupretre@gmail.com>
2016-10-05 21:56:05 +0200

    [js][html][about] Add social buttons

--- http/script/about.js
+++ http/script/about.js
@@ -15,4 +15,11 @@ LW.pages.about.init = function(params, $scope, $page) {
 		['MacAttac', "http://www.macattac.fr/2014/09/28/test-du-jeu-leek-wars/"]
 	]
 	$page.render()
+
+	// Twitter button
+	var twitter = function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],
+		p=/^http:/.test(d.location)?'http':'https';
+	if(!d.getElementById(id)){
+		js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+	fjs.parentNode.insertBefore(js,fjs);}}; twitter(document, 'script', 'twitter-wjs');
 }
