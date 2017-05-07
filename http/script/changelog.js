7266aad1a305f610aba80bf34f2a353afea2b409
Pilow <pierre.laupretre@gmail.com>
2017-05-07 22:43:46 +0200

    [changelog] Use lang files changelog data

--- http/script/changelog.js
+++ http/script/changelog.js
@@ -2,6 +2,18 @@ LW.pages.changelog.init = function(params, $scope, $page) {
 
 	_.get('changelog/get/' + _.lang.current, function(data) {
 
+		for (var d in data.changelog) {
+			var changes_data = _.lang.get('changelog', data.changelog[d].data)
+			data.changelog[d].changes = []
+			var changes_array = changes_data.split("\n")
+			for (var c in changes_array) {
+				var change = changes_array[c].replace('# ', '')
+				if (change.length > 0) {
+					data.changelog[d].changes.push(change)
+				}
+			}
+		}
+
 		$scope.changelog = data.changelog
 		$page.render()
 	})
