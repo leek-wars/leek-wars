66739601c95e4bfde13b4214481ce8c6ad5e4dbb
Pilow <pierre.laupretre@gmail.com>
2017-05-18 23:05:16 +0200

    [admin] Fix style and remove semicolons

--- http/script/admin_servers.js
+++ http/script/admin_servers.js
@@ -18,14 +18,14 @@ LW.pages.admin_servers.wsreceive = function(params) {
 
 	if (type == STATS_NEW_THREAD || type == STAT_UPDATE_THREAD) {
 		for (var t in data) {
-			var th = data[t];
-			updateThread(th.server, th.name, th.connected, th.task, th.task_start, th.generated, th.errors);
+			var th = data[t]
+			updateThread(th.server, th.name, th.connected, th.task, th.task_start, th.generated, th.errors)
 		}
 	}
 
 	if (type == STAT_UPDATE_TASK) {
-		data = data[0];
-		updateThreadTask(data.name, data.task, data.task_start, data.generated, data.errors);
+		data = data[0]
+		updateThreadTask(data.name, data.task, data.task_start, data.generated, data.errors)
 	}
 }
 
@@ -34,120 +34,110 @@ function updateThread(server, thread, connected, task, task_start, generated, er
 	// create server if not exists
 	if ($('#' + server).length == 0) {
 
-		var div = "<div id='" + server + "' class='server'>";
-		div += "<div class='load'><div></div></div>";
-		div += "<img src='" + LW.staticURL + "image/admin/server.png'></img><br>";
-		div += "<div class='name'>" + server + "<img src='" + LW.staticURL + "image/connected.png'></img> </div>";
-		div += "<div class='total-wrapper'>Total : <span class='total'></span></div>";
-		div += "<div class='threads'></div>";
-		div += "</div>";
+		var div = "<div id='" + server + "' class='server'>"
+		div += "<div class='load'><div></div></div>"
+		div += "<img src='" + LW.staticURL + "image/admin/server.png'></img><br>"
+		div += "<div class='name'>" + server + "<img src='" + LW.staticURL + "image/connected.png'></img> </div>"
+		div += "<div class='total-wrapper'>Total : <span class='total'></span></div>"
+		div += "<div class='threads'></div>"
+		div += "</div>"
 
-		$($('#servers .column')[rand3()]).append(div);
+		$($('#servers .column')[rand3()]).append(div)
 	}
 
-	var serverDiv = $('#' + server);
+	var serverDiv = $('#' + server)
 
 	if ($('#thread-' + thread).length == 0) {
 
-		var th = "<div id='thread-" + thread + "' class='thread'>";
-		th += "<div class='th-name'><img src='" + LW.staticURL + "/image/connected.png'></img>&nbsp;&nbsp;<b>" + thread + "</b></div>";
-		th += "<green>✔ <span class='generated' generated='" + generated + "'>" + generated + "</span></green>&nbsp; ";
-		th += "<red>✘ <span class='error' generated='" + errors + "'>" + errors + "</span></red><br>";
-		th += "► <span class='task'>" + task + "</span>";
-		th += "</div>";
-
-		serverDiv.find('.threads').append(th);
+		var th = "<div id='thread-" + thread + "' class='thread'>"
+		th += "<div class='th-name'><img src='" + LW.staticURL + "/image/connected.png'></img>&nbsp;&nbsp;<b>" + thread + "</b></div>"
+		th += "<green>✔ <span class='generated' generated='" + generated + "'>" + generated + "</span></green>&nbsp; "
+		th += "<red>✘ <span class='error' generated='" + errors + "'>" + errors + "</span></red><br>"
+		th += "► <span class='task'>" + task + "</span>"
+		th += "</div>"
+		serverDiv.find('.threads').append(th)
 	}
 
-	var threadDiv = $('#thread-' + thread);
+	var threadDiv = $('#thread-' + thread)
 	if (connected) {
-		threadDiv.find('img').attr('src', LW.staticURL + '/image/connected.png');
+		threadDiv.find('img').attr('src', LW.staticURL + '/image/connected.png')
 	} else {
-		threadDiv.find('img').attr('src', LW.staticURL + '/image/disconnected.png');
+		threadDiv.find('img').attr('src', LW.staticURL + '/image/disconnected.png')
 	}
 
-	updateServer(serverDiv);
+	updateServer(serverDiv)
 
 	setTimeout(function() {
-		updateColumns();
-	}, 500);
+		updateColumns()
+	}, 500)
 }
 
 function updateThreadTask(thread, task, task_start, generated, errors) {
 
-	var threadDiv = $('#thread-' + thread);
+	var threadDiv = $('#thread-' + thread)
 
-	threadDiv.find('.task').text(task);
-	threadDiv.find('.generated').attr('generated', generated);
+	threadDiv.find('.task').text(task)
+	threadDiv.find('.generated').attr('generated', generated)
 	threadDiv.find('.generated').html(_.format.number(generated))
 
-	updateServer(threadDiv.closest('.server'));
+	updateServer(threadDiv.closest('.server'))
 }
 
 function updateServer(server) {
-
 	// Total
-	var total = 0;
+	var total = 0
 	server.find('.thread').each(function() {
-		total += parseInt($(this).find('.generated').attr('generated'));
-	});
-	server.find('.total').html(_.format.number(total));
+		total += parseInt($(this).find('.generated').attr('generated'))
+	})
+	server.find('.total').html(_.format.number(total))
 
 	// Load
-	var threads = server.find('.thread').length;
-	var active = 0;
+	var threads = server.find('.thread').length
+	var active = 0
 	server.find('.thread').each(function() {
-		if ($(this).find('.task').text() == "") active++;
-	});
-	server.find('.load div').css('margin-top', (active / threads) * 136);
+		if ($(this).find('.task').text() == "") active++
+	})
+	server.find('.load div').css('margin-top', (active / threads) * 136)
 }
 
 function rand3() {
-	return Math.floor(Math.random() * 3);
+	return Math.floor(Math.random() * 3)
 }
 
 function updateColumns() {
 
-	var servers = [];
-
+	var servers = []
 	for (var i = 0; i < 3; ++i) {
-
-		var c = $($('#servers .column')[i]);
-
-		var h = c.height();
-
+		var c = $($('#servers .column')[i])
+		var h = c.height()
 		c.find('.server').each(function() {
-			servers.push({elem: $(this), size: $(this).height()});
-		});
-
-		c.empty();
+			servers.push({elem: $(this), size: $(this).height()})
+		})
+		c.empty()
 	}
-
-	var secure = 0;
-
+	var secure = 0
 	while (servers.length > 0 && secure++ < 100) {
-		var max = -1;
-		var maxi;
-		var maxix;
+		var max = -1
+		var maxi
+		var maxix
 		for (var s in servers) {
 			if (servers[s].size > max) {
-				max = servers[s].size;
-				maxi = servers[s];
-				maxix = s;
+				max = servers[s].size
+				maxi = servers[s]
+				maxix = s
 			}
 		}
+		servers.splice(maxix, 1)
 
-		servers.splice(maxix, 1);
-
-		var min = 999999;
-		var mini;
+		var min = 999999
+		var mini
 		for (var i = 0; i < 3; ++i) {
-			var c = $($('#servers .column')[i]);
+			var c = $($('#servers .column')[i])
 			if (c.height() < min) {
-				min = c.height();
-				mini = c;
+				min = c.height()
+				mini = c
 			}
 		}
-		mini.append(maxi.elem);
+		mini.append(maxi.elem)
 	}
 }
