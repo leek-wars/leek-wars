769ed347ac6480157f5cba0a219fd990c06c434f
Pilow <pierre.laupretre@gmail.com>
2017-05-26 14:30:15 +0200

    [notifications] Fix notification date

--- http/script/notifications.js
+++ http/script/notifications.js
@@ -4,7 +4,7 @@ LW.pages.notifications.init = function(params, $scope, $page) {
 
 		for (var n in data.notifications) {
 			var notif = LW.notifications.getData(data.notifications[n])
-			notif.date = LW.util.formatDuration(notif.date)
+			notif.formatted_date = LW.util.formatDuration(notif.date)
 			notif.view = _.view.render('main.notification', notif)
 			data.notifications[n] = notif
 		}
