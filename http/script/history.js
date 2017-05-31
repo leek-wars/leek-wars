d377899598ed87f0f25f8c3cff479acd0592a0b2
Pilow <pierre.laupretre@gmail.com>
2017-05-31 21:00:23 +0200

    [history] Add period selection to filter fights

--- http/script/history.js
+++ http/script/history.js
@@ -21,6 +21,38 @@ LW.pages.history.init = function(params, $scope, $page) {
 			})
 		}
 
+		var select_period = function(period) {
+			var now = Date.now() / 1000
+			var midnignt = new Date()
+			midnignt.setHours(0, 0, 0, 0)
+			var day = 24 * 3600
+			var start_date = function() {
+				if (period == '24h') return now - day
+				if (period == 'today') return midnignt.getTime() / 1000
+				if (period == '2days') return now - 2 * day
+				if (period == '1week') return now - 7 * day
+			}()
+			filter_fights(start_date)
+		}
+
+		var filter_fights = function(start_date) {
+			_.log("Filter fights ", start_date)
+			var count = 0
+			$('#history-page .fight').each(function() {
+				var visible = parseInt($(this).attr('date')) >= start_date
+				$(this).toggle(visible)
+				if (visible) count++
+			})
+			$('#history-page .n-fights').html(_.lang.get('history', 'n_fights', count))
+		}
+
+		$('#history-page .period').click(function() {
+			$('#history-page .period').removeClass('selected')
+			$(this).addClass('selected')
+			var period = $(this).attr('period')
+			select_period(period)
 		})
+
+		select_period('1week')
 	})
 }
