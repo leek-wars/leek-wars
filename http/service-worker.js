f8caef4e04ec388e5cca414eb6c5c2d34c242bec
Pilow <pierre.laupretre@gmail.com>
2017-06-20 22:59:28 +0200

    [sw] Remove useless fetch method

--- http/service-worker.js
+++ http/service-worker.js
@@ -25,20 +25,6 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
 
         // IMPORTANT: Clone the request. A request is a stream and
         // can only be consumed once. Since we are consuming this
