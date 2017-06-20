6b046e58816c6040a9449f0d6275237aac93c2a5
Pilow <pierre.laupretre@gmail.com>
2017-06-20 22:19:49 +0200

    [lib] Move Class class in library file

--- http/script/_.js
+++ http/script/_.js
@@ -66,6 +66,26 @@ _.init = function(params) {
 	_.logW = function() {}
 }
 
+var Class = function() {
+    this.initialize && this.initialize.apply(this, arguments)
+}
+
+Class.extend = function(base, childPrototype) {
+    var parent = this
+    var child = function() {
+        parent.apply(this, arguments)
+        if (this.__construct) this.__construct(arguments)
+    }
+    child.extend = parent.extend
+    var Surrogate = function() {}
+    Surrogate.prototype = parent.prototype
+    child.prototype = new Surrogate
+    for (var key in base) child[key] = base[key]
+    var instance = new childPrototype
+    for (var key in instance) child.prototype[key] = instance[key]
+    return child
+}
+
 /*
  * Generic functions
  */
