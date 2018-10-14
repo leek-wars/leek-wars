277391957352cfda9e492df60c2229cdc09df577
Pilow <pierre.laupretre@gmail.com>
2018-10-14 13:46:58 +0800

    [bar] Reduce bar shadow

--- src/component/app/bar.vue
+++ src/component/app/bar.vue
@@ -82,7 +82,7 @@
 		word-break: break-all;
 		white-space: nowrap;
 		display: flex;
-		box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12);
+		box-shadow: 0 2px 2px 0 rgba(0,0,0,.07);
 	}
 	#app:not(.connected) #app-bar {
 		display: none;
