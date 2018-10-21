a4f241a61421338f06e6c34c69762da3909830d3
Pilow <pierre.laupretre@gmail.com>
2018-10-21 14:35:32 +0800

    [tutorial] Fix tutorial translations #355

--- src/component/tutorial/tutorial.vue
+++ src/component/tutorial/tutorial.vue
@@ -15,10 +15,7 @@
 
 					<center><h2>{{ $t('welcome') }}</h2></center>
 					<br>
-
-					<span class="justify">
-						{{ $t('intro') }}
-					</span>
+					<span class="justify" v-html="$t('intro')"></span>
 				</div>
 			</div>
 		</div>
@@ -97,25 +94,25 @@
 
 				<div class="wrapper">
 
-					<p>{{ $t('presentation_1') }}</p>
+					<p v-html="$t('presentation_1')"></p>
 					<p>{{ $t('presentation_2') }}</p>
 
 					<h3>{{ $t('equip_a_weapon') }}</h3>
 
 					<p>{{ $t('buy_a_weapon') }}</p>
-					<p>{{ $t('buy_a_weapon_2') }}</p>
+					<p v-html="$t('buy_a_weapon_2')"></p>
 					<p>{{ $t('buy_a_weapon_3') }}</p>
 
 					<h3>{{ $t('what_is_leekscript') }}</h3>
 
-					<p>{{ $t('what_is_leekscript_1') }}</p>
+					<p v-html="$t('what_is_leekscript_1')"></p>
 					<p>{{ $t('what_is_leekscript_2') }}</p>
 
 					<h3>{{ $t('what_is_ai') }}</h3>
 
-					<p>{{ $t('what_is_ai_1') }}</p>
-					<p>{{ $t('what_is_ai_2') }}</p>
-					<p>{{ $t('what_is_ai_3') }}</p>
+					<p v-html="$t('what_is_ai_1')"></p>
+					<p v-html="$t('what_is_ai_2')"></p>
+					<p v-html="$t('what_is_ai_3')"></p>
 				</div>
 			</div>
 		</div>
@@ -132,7 +129,7 @@
 
 					<h3>{{ $t('editor') }}</h3>
 
-					<p>{{ $t('editor_1') }}</p>
+					<p v-html="$t('editor_1')"></p>
 
 					<screen src="/image/tuto/editor.jpg" :legend="$t('editor_image_1')"></screen>
 
@@ -142,7 +139,7 @@
 
 					<screen src="/image/tuto/editor_ia.jpg" :legend="$t('editor_image_2')"></screen>
 
-					<p>{{ $t('editor_5') }}</p>
+					<p v-html="$t('editor_5')"></p>
 
 					<p>{{ $t('editor_6') }}<br>
 						<ul>
@@ -158,17 +155,18 @@
 
 					<p>{{ $t('editor_8') }}
 						<ul>
-							<li>{#editor.shortcut_1}</li>
-							<li>{#editor.shortcut_2}</li>
-							<li>{#editor.shortcut_3}</li>
-							<li>{#editor.shortcut_4}</li>
-							<li>{#editor.shortcut_5}</li>
-							<li>{#editor.shortcut_6}</li>
-							<li>{#editor.shortcut_7}</li>
+							<li v-html="$t('editor.shortcut_1')"></li>
+							<li v-html="$t('editor.shortcut_2')"></li>
+							<li v-html="$t('editor.shortcut_3')"></li>
+							<li v-html="$t('editor.shortcut_4')"></li>
+							<li v-html="$t('editor.shortcut_5')"></li>
+							<li v-html="$t('editor.shortcut_6')"></li>
+							<li v-html="$t('editor.shortcut_7')"></li>
 						</ul>
 					</p>
 
-					<p>{{ $t('editor_9') }}
+					<p>
+						<span v-html="$t('editor_9')"></span>
 						<br>
 						<br>
 						{{ $t('editor_10') }}
@@ -199,7 +197,7 @@
 
 					<h3>{{ $t('documentation') }}</h3>
 
-					<p>{{ $t('doc1') }}</p>
+					<p v-html="$t('doc_1')"></p>
 
 					<screen src="/image/tuto/documentation.jpg" :legend="$t('the_documentation')"></screen>
 
@@ -213,15 +211,15 @@
 
 					<p>{{ $t('doc_6') }}
 						<ul>
-							<li>{{ $t('doc_math') }}</li>
-							<li>{{ $t('doc_strings') }}</li>
-							<li>{{ $t('doc_arrays') }}</li>
-							<li>{{ $t('doc_leeks') }}</li>
-							<li>{{ $t('doc_weapons') }}</li>
-							<li>{{ $t('doc_chips') }}</li>
-							<li>{{ $t('doc_map') }}</li>
-							<li>{{ $t('doc_fight') }}</li>
-							<li>{{ $t('doc_util') }}</li>
+							<li v-html="$t('doc_math')"></li>
+							<li v-html="$t('doc_strings')"></li>
+							<li v-html="$t('doc_arrays')"></li>
+							<li v-html="$t('doc_leeks')"></li>
+							<li v-html="$t('doc_weapons')"></li>
+							<li v-html="$t('doc_chips')"></li>
+							<li v-html="$t('doc_map')"></li>
+							<li v-html="$t('doc_fight')"></li>
+							<li v-html="$t('doc_util')"></li>
 						</ul>
 					</p>
 
@@ -277,7 +275,7 @@
 					<p>{{ $t('var_14') }}</p>
 					<lw-code :code="$t('var_15')" />
 
-					<p>{{ $t('var_16') }}</p>
+					<p v-html="$t('var_16')"></p>
 
 					<p>{{ $t('var_17') }}</p>
 
@@ -293,7 +291,7 @@
 
 					<p>{{ $t('var_23') }}</p>
 
-					<p>{{ $t('var_24') }}</p>
+					<p v-html="$t('var_24')"></p>
 
 					<lw-code :code="$t('var_25')" />
 
@@ -320,7 +318,7 @@
 
 					<lw-code :code="$t('var_37')" />
 
-					<p>{{ $t('var_38') }}</p>
+					<p v-html="$t('var_38')"></p>
 
 					<lw-code :code="$t('var_39')" />
 
@@ -335,7 +333,7 @@
 
 					<p>{{ $t('var_44') }}</p>
 
-					<p>{{ $t('var_45') }}</p>
+					<p v-html="$t('var_45')"></p>
 
 					<p>{{ $t('var_46') }}</p>
 
@@ -347,11 +345,11 @@
 
 					<p>{{ $t('var_50') }}</p>
 
-					<p>{{ $t('var_51') }}</p>
+					<p v-html="$t('var_51')"></p>
 
 					<lw-code :code="$t('var_52')" />
 
-					<p>{{ $t('var_53') }}</p>
+					<p v-html="$t('var_53')"></p>
 
 					<h3>{{ $t('arrays') }}</h3>
 
@@ -365,7 +363,7 @@
 
 					<lw-code :code="$t('arr_5')" />
 
-					<p>{{ $t('arr_6') }}</p>
+					<p v-html="$t('arr_6')"></p>
 
 					<lw-code :code="$t('arr_7')" />
 
@@ -374,9 +372,7 @@
 					<lw-code :code="$t('arr_9')" />
 
 					<p>{{ $t('arr_10') }}</p>
-					<table>
-						{{ $t('arr_11') }}
-					</table>
+					<table v-html="$t('arr_11')"></table>
 
 					<p>{{ $t('arr_12') }}</p>
 
@@ -401,7 +397,7 @@
 
 				<div class="wrapper">
 
-					<p>{{ $t('cond_1') }}</p>
+					<p v-html="$t('cond_1')"></p>
 
 					<h3>{{ $t('cond_2') }}</h3>
 
@@ -413,7 +409,7 @@
 
 					<p>{{ $t('cond_6') }}</p>
 
-					<p>{{ $t('cond_7') }}</p>
+					<p v-html="$t('cond_7')"></p>
 
 					<p>{{ $t('cond_8') }}</p>
 
@@ -427,20 +423,16 @@
 
 					<p>{{ $t('cond_13') }}</p>
 
-					<table>
-						{{ $t('cond_14') }}
-					</table>
+					<table v-html="$t('cond_14')"></table>
 
 					<p>{{ $t('cond_15') }}</p>
-					<table>
-						{{ $t('cond_16') }}
-					</table>
+					<table v-html="$t('cond_16')"></table>
 
 					<p>{{ $t('cond_17') }}</p>
 
 					<lw-code :code="$t('cond_18')" />
 
-					<p>{{ $t('cond_19') }}</p>
+					<p v-html="$t('cond_19')"></p>
 
 					<h3>{{ $t('cond_20') }}</h3>
 
@@ -464,12 +456,13 @@
 
 					<p>{{ $t('cond_30') }}</p>
 
-					<p>{{ $t('cond_31') }}
+					<p>
+						<span v-html="$t('cond_31')"></span>
 						<ul>
-							<li>{{ $t('cond_32') }}</li>
-							<li>{{ $t('cond_33') }}</li>
-							<li>{{ $t('cond_34') }}</li>
-							<li>{{ $t('cond_35') }}</li>
+							<li v-html="$t('cond_32')"></li>
+							<li v-html="$t('cond_33')"></li>
+							<li v-html="$t('cond_34')"></li>
+							<li v-html="$t('cond_35')"></li>
 						</ul>
 					</p>
 				</div>
@@ -499,19 +492,20 @@
 
 					<lw-code :code="$t('loops_7')" />
 
-					<p>{{ $t('loops_8') }}</p>
+					<p v-html="$t('loops_8')"></p>
 
 					<h3>{{ $t('loops_9') }}</h3>
 
-					<p>{{ $t('loops_10') }}</p>
+					<p v-html="$t('loops_10')"></p>
 
 					<lw-code :code="$t('loops_11')" />
 
 					<h3>{{ $t('loops_12') }}</h3>
 
-					<p>{{ $t('loops_13') }}</p>
+					<p v-html="$t('loops_13')"></p>
 
-					<p>{{ $t('loops_14') }}
+					<p>
+						<span v-html="$t('loops_14')"></span>
 						<ul>
 							<li>{{ $t('loops_15') }}</li>
 							<li>{{ $t('loops_16') }}</li>
@@ -519,23 +513,22 @@
 						</ul>
 					</p>
 
-					<p>{{ $t('loops_18') }}</p>
+					<p v-html="$t('loops_18')"></p>
 
 					<lw-code :code="$t('loops_19')" />
 
-
-					<p>{{ $t('loops_20') }}</p>
+					<p v-html="$t('loops_20')"></p>
 
 					<p>{{ $t('loops_21') }}</p>
 
 					<lw-code :code="$t('loops_22')" />
 
-					<p>{{ $t('loops_23') }}</p>
+					<p v-html="$t('loops_23')"></p>
 
 					<lw-code :code="$t('loops_24')" />
 					<p>{{ $t('loops_25') }}</p>
 
-					<p>{{ $t('loops_26') }}</p>
+					<p v-html="$t('loops_26')"></p>
 
 					<lw-code :code="$t('loops_27')" />
 				</div>
@@ -556,23 +549,23 @@
 
 					<h3>{{ $t('fun_2') }}</h3>
 
-					<p>{{ $t('fun_3') }}</p>
+					<p v-html="$t('fun_3')"></p>
 
 					<lw-code :code="$t('fun_4')" />
 
-					<p>{{ $t('fun_5') }}</p>
+					<p v-html="$t('fun_5')"></p>
 
 					<lw-code :code="$t('fun_6')" />
 
-					<p>{{ $t('fun_7') }}</p>
+					<p v-html="$t('fun_7')"></p>
 
 					<lw-code :code="$t('fun_8')" />
 
-					<p>{{ $t('fun_9') }}</p>
+					<p v-html="$t('fun_9')"></p>
 
-					<p>{{ $t('fun_10') }}</p>
+					<p v-html="$t('fun_10')"></p>
 
-					<p>{{ $t('fun_11') }}</p>
+					<p v-html="$t('fun_11')"></p>
 
 					<lw-code :code="$t('fun_12')" />
 
@@ -582,17 +575,17 @@
 
 					<p>{{ $t('fun_15') }}</p>
 
-					<p>{{ $t('fun_16') }}</p>
+					<p v-html="$t('fun_16')"></p>
 
 					<h3>{{ $t('fun_17') }}</h3>
 
-					<p>{{ $t('fun_18') }}</p>
+					<p v-html="$t('fun_18')"></p>
 
-					<p>{{ $t('fun_19') }}</p>
+					<p v-html="$t('fun_19')"></p>
 
 					<lw-code :code="$t('fun_20')" />
 
-					<p>{{ $t('fun_21') }}</p>
+					<p v-html="$t('fun_21')"></p>
 
 					<lw-code :code="$t('fun_22')" />
 
@@ -608,7 +601,7 @@
 
 					<p>{{ $t('fun_28') }}</p>
 
-					<p>{{ $t('fun_29') }}</p>
+					<p v-html="$t('fun_29')"></p>
 
 					<p>{{ $t('fun_30') }}</p>
 
@@ -659,92 +652,92 @@
 
 				<div class="wrapper">
 
-					<p>{{ $t('fight_intro') }}</p>
+					<p v-html="$t('fight_intro')"></p>
 
 					<h2>{{ $t('fight_sys') }}</h2>
 
-					<p>{{ $t('fight_sys_1') }}</p>
+					<p v-html="$t('fight_sys_1')"></p>
 
 					<h3>{{ $t('fight_sys_2') }}</h3>
 
-					<p>{{ $t('fight_sys_3') }}</p>
+					<p v-html="$t('fight_sys_3')"></p>
 
-					<p>{{ $t('fight_sys_4') }}</p>
+					<p v-html="$t('fight_sys_4')"></p>
 
 					<lw-code :code="$t('fight_sys_5')" />
 
 					<h3>{{ $t('fight_sys_6') }}</h3>
-					<p>{{ $t('fight_sys_7') }}</p>
+					<p v-html="$t('fight_sys_7')"></p>
 
-					<p>{{ $t('fight_sys_8') }}</p>
+					<p v-html="$t('fight_sys_8')"></p>
 
 					<lw-code :code="$t('fight_sys_9')" />
 
-					<p>{{ $t('fight_sys_10') }}</p>
+					<p v-html="$t('fight_sys_10')"></p>
 
 					<lw-code :code="$t('fight_sys_11')" />
 
-					<p>{{ $t('fight_sys_12') }}</p>
+					<p v-html="$t('fight_sys_12')"></p>
 
 					<lw-code :code="$t('fight_sys_13')" />
 
 					<h3>{{ $t('fight_ground') }}</h3>
 
-					<p>{{ $t('fight_ground_1') }}</p>
+					<p v-html="$t('fight_ground_1')"></p>
 
 					<screen src="/image/tuto/grid_cells.jpg" :legend="$t('fight_ground_2')"></screen>
 
-					<p>{{ $t('fight_ground_3') }}</p>
+					<p v-html="$t('fight_ground_3')"></p>
 
 					<lw-code :code="$t('fight_ground_4')" />
 
 					<h3>{{ $t('fight_ground_5') }}</h3>
 
-					<p>{{ $t('fight_ground_6') }}</p>
+					<p v-html="$t('fight_ground_6')"></p>
 
 					<lw-code :code="$t('fight_ground_7')" />
 
-					<p>{{ $t('fight_ground_8') }}</p>
+					<p v-html="$t('fight_ground_8')"></p>
 
 					<lw-code :code="$t('fight_ground_9')" />
 
-					<p>{{ $t('fight_ground_10') }}</p>
+					<p v-html="$t('fight_ground_10')"></p>
 
 					<h3>{{ $t('fight_ground_11') }}</h3>
 
-					<p>{{ $t('fight_ground_12') }}</p>
+					<p v-html="$t('fight_ground_12')"></p>
 
 					<lw-code :code="$t('fight_ground_13')" />
 
 					<p><ul>
-						<li>{{ $t('fight_ground_14') }}</li>
-						<li>{{ $t('fight_ground_15') }}</li>
-						<li>{{ $t('fight_ground_16') }}</li>
-						<li>{{ $t('fight_ground_17') }}</li>
-						<li>{{ $t('fight_ground_18') }}</li>
+						<li v-html="$t('fight_ground_14')"></li>
+						<li v-html="$t('fight_ground_15')"></li>
+						<li v-html="$t('fight_ground_16')"></li>
+						<li v-html="$t('fight_ground_17')"></li>
+						<li v-html="$t('fight_ground_18')"></li>
 					</ul></p>
 
-					<p>{{ $t('fight_ground_19') }}</p>
+					<p v-html="$t('fight_ground_19')"></p>
 
 					<lw-code :code="$t('fight_ground_20')" />
 
 					<h3>{{ $t('fight_weapons') }}</h3>
 
-					<p>{{ $t('fight_weapons_2') }}</p>
+					<p v-html="$t('fight_weapons_2')"></p>
 
 					<lw-code :code="$t('fight_weapons_3')" />
 
-					<p>{{ $t('fight_weapons_4') }}</p>
+					<p v-html="$t('fight_weapons_4')"></p>
 
 					<h3>{{ $t('fight_weapons_5') }}</h3>
 
-					<p>{{ $t('fight_weapons_6') }}</p>
+					<p v-html="$t('fight_weapons_6')"></p>
 
 					<lw-code :code="$t('fight_weapons_7')" />
 
 					<h3>{{ $t('fight_chips') }}</h3>
 
-					<p>{{ $t('fight_chips_1') }}</p>
+					<p v-html="$t('fight_chips_1')"></p>
 
 					<lw-code :code="$t('fight_chips_2')" />
 
@@ -754,7 +747,7 @@
 
 					<p>{{ $t('fight_chips_5') }}</p>
 
-					<p>{{ $t('fight_chips_6') }}</p>
+					<p v-html="$t('fight_chips_6')"></p>
 
 					<p>{{ $t('fight_chips_7') }}</p>
 
@@ -767,36 +760,35 @@
 
 					<h3>{{ $t('fight_move') }}</h3>
 
-					<p>{{ $t('fight_move_1') }}</p>
+					<p v-html="$t('fight_move_1')"></p>
 
 					<h3>{{ $t('fight_move_2') }}</h3>
 
-					<p>{{ $t('fight_move_3') }}</p>
+					<p v-html="$t('fight_move_3')"></p>
 
 					<lw-code :code="$t('fight_move_4')" />
 
-					<p>{{ $t('fight_move_5') }}</p>
+					<p v-html="$t('fight_move_5')"></p>
 
-					<p>{{ $t('fight_move_6') }}</p>
+					<p v-html="$t('fight_move_6')"></p>
 
 					<lw-code :code="$t('fight_move_7')" />
 
-					<p>{{ $t('fight_move_8') }}</p>
+					<p v-html="$t('fight_move_8')"></p>
 
 					<h3>{{ $t('fight_move_9') }}</h3>
 
-					<p>{{ $t('fight_move_10') }}</p>
+					<p v-html="$t('fight_move_10')"></p>
 
 					<h3>{{ $t('fight_move_11') }}</h3>
 
-					<p>{{ $t('fight_move_12') }}</p>
+					<p v-html="$t('fight_move_12')"></p>
 
 					<lw-code :code="$t('fight_move_13')" />
 
-
 					<h3>{{ $t('fight_speak') }}</h3>
 
-					<p>{{ $t('fight_speak_1') }}</p>
+					<p v-html="$t('fight_speak_1')"></p>
 
 					<lw-code :code="$t('fight_speak_2')" />
 				</div>
@@ -814,18 +806,18 @@
 
 				<div class="wrapper">
 
-					<p>{{ $t('advanced_intro') }}</p>
+					<p v-html="$t('advanced_intro')"></p>
 
 					<h3>{{ $t('advanced_order') }}</h3>
 
-					<p>{{ $t('advanced_order_1') }}</p>
+					<p v-html="$t('advanced_order_1')"></p>
 
 					<lw-code :code="$t('advanced_order_2')" />
 
 					<br>
 					<h3>{{ $t('advanced_order_3') }}</h3>
 
-					<p>{{ $t('advanced_order_4') }}</p>
+					<p v-html="$t('advanced_order_4')"></p>
 
 					<lw-code :code="$t('advanced_order_5')" />
 
@@ -835,7 +827,7 @@
 
 					<lw-code :code="$t('advanced_ref_2')" />
 
-					<p>{{ $t('advanced_ref_3') }}</p>
+					<p v-html="$t('advanced_ref_3')"></p>
 
 					<lw-code :code="$t('advanced_ref_4')" />
 
@@ -865,7 +857,7 @@
 
 					<h3 id="includes">{{ $t('advanced_include') }}</h3>
 
-					<p>{{ $t('advanced_include_1') }}</p>
+					<p v-html="$t('advanced_include_1')"></p>
 
 					<lw-code :code="$t('advanced_include_2')" />
 
@@ -895,20 +887,17 @@
 					<br>
 					<h3 id="triple_equals">{{ $t('advanced_triple_equal') }}</h3>
 
-					<p>{{ $t('advanced_triple_equal_1') }}</p>
+					<p v-html="$t('advanced_triple_equal_1')"></p>
 
-					<table>
-						{{ $t('advanced_triple_equal_2') }}
-					</table>
+					<table v-html="$t('advanced_triple_equal_2')"></table>
 
-					<p>{{ $t('advanced_triple_equal_3') }}</p>
+					<p v-html="$t('advanced_triple_equal_3')"></p>
 
 					<table class="sign-table">
 						<tr>
 							<td>===</td>
 							<td v-for="value in values" :key="value">{{ value }}</td>
 						</tr>
-
 						<tr v-for="value1 in values" :key="value1">
 							<td>{{ value1 }}</td>
 							<td v-for="(value2, j) in values" :key="j">
@@ -917,7 +906,7 @@
 						</tr>
 					</table>
 
-					<p>{{ $t('advanced_triple_equal_4') }}</p>
+					<p v-html="$t('advanced_triple_equal_4')"></p>
 
 					<table class="sign-table">
 						<tr>
@@ -932,11 +921,11 @@
 						</tr>
 					</table>
 
-					<p>{{ $t('advanced_triple_equal_5') }}</p>
+					<p v-html="$t('advanced_triple_equal_5')"></p>
 
-					<p>{{ $t('advanced_triple_equal_6') }}</p>
+					<p v-html="$t('advanced_triple_equal_6')"></p>
 
-					<p>{{ $t('advanced_triple_equal_7') }}</p>
+					<p v-html="$t('advanced_triple_equal_7')"></p>
 				</div>
 			</div>
 		</div>
@@ -1045,12 +1034,12 @@
 		margin: 10px auto;
 		background: white;
 	}
-	table td {
+	table /deep/ td, table /deep/ th {
 		border: 1px solid #aaa;
 		padding: 3px 10px;
 		text-align: center;
 	}
-	a {
+	a, .content /deep/ a {
 		color: #5fad1b;
 	}
 	.sign-table {
