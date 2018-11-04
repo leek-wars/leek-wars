487076fe051f054a1382080c4984cb354ea1c15b
Pilow <pierre.laupretre@gmail.com>
2018-11-04 23:18:22 +0800

    [sw] Add fetch event to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,27 @@
+self.addEventListener('fetch', event => {
+	// Let the browser do its default thing
+	// for non-GET requests.
+	if (event.request.method != 'GET') return;
+	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+		return;
+	}
+
+	// Prevent the default, and handle the request ourselves.
+	event.respondWith(async function() {
+		// Try to get the response from a cache.
+		const cache = await caches.open('dynamic-v1');
+		const cachedResponse = await cache.match(event.request);
+		if (cachedResponse) {
+			// If we found a match in the cache, return it, but also
+			// update the entry in the cache in the background.
+			event.waitUntil(cache.add(event.request));
+			return cachedResponse;
+		}
+		// If we didn't find a match in the cache, use the network.
+		return fetch(event.request);
+	}())
+})
+
 self.addEventListener('push', event => {
 	var icon = null
 	var title = "Notification de Leek Wars"
