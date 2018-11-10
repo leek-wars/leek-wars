6ed139411fe1722559105eccf6e15ca9fc0dc6c9
Pilow <pierre.laupretre@gmail.com>
2018-11-11 01:41:10 +0800

    [translation] Improve translations loading #425 #413

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
-    configureWebpack: {
+	configureWebpack: {
 		plugins: [
 			// new BundleAnalyzerPlugin()
 		]
+	},
+	pages: {
+		index: {entry: 'src/main-fr'},
+		'app-fr': {entry: 'src/main-fr'},
+		'app-en': {entry: 'src/main-en'}
 	},
     chainWebpack: config => {
 		config.module
@@ -12,6 +17,9 @@ module.exports = {
 			.use('url-loader')
 			.loader('url-loader')
 			.tap(options => Object.assign(options, { limit: -1 }))
+		if (process.env.VUE_MODE === 'build') {
+			config.entryPoints.delete('index')
+		}
 	},
     pwa: {
 		name: 'Leek Wars',
