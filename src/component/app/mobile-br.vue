ad17d0e0c7a27fbf19a37962789a0b36b02172b1
Pilow <pierre.laupretre@gmail.com>
2019-03-24 12:14:57 +0100

    [br] Add BR view for mobile

---
+++ src/component/app/mobile-br.vue
@@ -0,0 +1,26 @@
+<template>
+	<router-link v-if="LeekWars.battleRoyale.enabled" to="/garden/battle-royale" class="br">
+		BR <b class="progress">{{ LeekWars.battleRoyale.progress }} / 10</b>
+	</router-link>
+</template>
+
+<script lang="ts">
+	import { Component, Vue } from 'vue-property-decorator'
+	@Component({})
+	export default class MobileBR extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+	.br {
+		z-index: 100;
+		position: fixed;
+		top: 50%;
+		right: -25px;
+		background: #2a2a2a;
+		color: white;
+		transform: rotate(-90deg);
+		padding: 7px 10px;
+		border-top-left-radius: 6px;
+		border-top-right-radius: 6px;
+	}
+</style>
