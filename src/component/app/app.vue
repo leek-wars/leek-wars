8f94779bec7cc8794bad9b24ebe9b9c947dd98d8
Pilow <pierre.laupretre@gmail.com>
2019-03-24 12:14:40 +0100

    [menu] Don't collapse menu in mobile mode

--- src/component/app/app.vue
+++ src/component/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="app" :class="{ connected: $store.state.connected, app: LeekWars.mobile, 'social-collapsed': LeekWars.socialCollapsed, 'menu-expanded': LeekWars.menuExpanded, sfw: LeekWars.sfw, 'menu-collapsed': LeekWars.menuCollapsed }" data-app="true" @mousemove="consoleMouseMove" @mouseup="consoleMouseUp">
+	<div id="app" :class="{ connected: $store.state.connected, app: LeekWars.mobile, 'social-collapsed': LeekWars.socialCollapsed, 'menu-expanded': LeekWars.menuExpanded, sfw: LeekWars.sfw, 'menu-collapsed': !LeekWars.mobile && LeekWars.menuCollapsed }" data-app="true" @mousemove="consoleMouseMove" @mouseup="consoleMouseUp">
 	
 		<div :class="{visible: LeekWars.dark > 0}" :style="{opacity: LeekWars.dark}" class="dark" @click="darkClick"></div>
 	
