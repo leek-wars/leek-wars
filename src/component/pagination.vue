6e8f46a019f0834804064a6f731cb77653aae50d
Pilow <pierre.laupretre@gmail.com>
2019-03-31 00:44:19 +0100

    [pagination] Add setting to use query style argument for page

--- src/component/pagination.vue
+++ src/component/pagination.vue
@@ -3,7 +3,7 @@
 		<router-link v-if="start > 1" :to="url">1</router-link>
 		<span v-if="start > 1">...</span>
 
-		<router-link v-for="i in (end - start + 1)" :key="i" :to="url + (start + i === 2 ? '' : '/page-' + (start + i - 1))" :class="{current: i + start - 1 === current}">{{ start + i - 1 }}</router-link>	
+		<router-link v-for="i in (end - start + 1)" :key="i" :to="url + (start + i === 2 ? '' : page + (start + i - 1))" :class="{current: i + start - 1 === current}">{{ start + i - 1 }}</router-link>	
 
 		<span v-if="end < total">...</span>
 		<router-link v-if="end < total" :to="url + '/page-' + total">{{ total }}</router-link>
@@ -20,9 +20,11 @@
 		@Prop({required: true}) current!: number
 		@Prop({required: true}) total!: number
 		@Prop({required: true}) url!: string
+		@Prop() query!: boolean
 		get center(): number { return Math.max(5, Math.min(this.total - 4, this.current)) }
 		get start(): number { return this.center - 4 }
 		get end(): number { return Math.min(this.total, this.center + 4) }
+		get page(): string { return this.query ? '&page=' : '/page-' }
 	}
 </script>
 
