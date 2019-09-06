bc3d672946a1143d396ff6505aec501ae1104e61
Pilow <pierre.laupretre@gmail.com>
2019-09-06 23:32:28 +0200

    [bank] Improve & fix bank error messages

--- src/component/bank/bank-validate.vue
+++ src/component/bank/bank-validate.vue
@@ -4,20 +4,20 @@
 			<div class="page-header page-bar">
 				<h1 v-html="$t('payment_success', [vendor])"></h1>
 			</div>
-			<panel class="center">
+			<panel class="first center">
 				<br>
 				<h4 v-html="$t('you_earn_n_crystals', [crystals])"></h4>
 				<br>
 			</panel>
 		</template>
-		<div v-else>
+		<div v-else-if="error">
 			<div class="page-header page-bar">
 				<h1 v-html="$t('payment_fail', [vendor])"></h1>
 			</div>
-			<panel class="center">
+			<panel class="first center">
 				<br>
 				<img src="/image/notgood.png"><br><br>
-				<h4>{{ $t('payment_fail_reason', [reason]) }}</h4> 
+				<h4>{{ $t('payment_fail_reason', [$t(reason)]) }}</h4>
 				<br>
 				<router-link to="/bank"><v-btn>{{ $t('back_to_bank') }}</v-btn></router-link>
 			</panel>
@@ -34,6 +34,7 @@
 	})
 	export default class BankValidate extends Vue {
 		@Prop() success!: boolean
+		error: boolean = false
 		reason: string | null = null
 		crystals: number = 0
 		vendor: string | null = null
@@ -47,26 +48,28 @@
 			this.vendor = 'vendor' in this.$route.params ? this.$route.params.vendor : ''
 			LeekWars.setTitle(this.success ? this.$t('payment_success', [this.vendor]) : this.$t('payment_fail', [this.vendor]))
 			if (this.success !== undefined) {
+				this.error = true
 				return
 			}
 			const url = document.location!.search
-			const match = url.match(/\?paymentId=(.*?)&token=(.*?)&PayerID=(.*?)$/)
-			if (match) {
-				const payment_id = match[1]
-				const token = match[2]
-				const payer_id = match[3]
+			if (LeekWars.objectSize(this.$route.query)) { // PayPal return url: /\?paymentId=(.*?)&token=(.*?)&PayerID=(.*?)$/
+				const payment_id = this.$route.query.paymentId
+				const token = this.$route.query.token
+				const payer_id = this.$route.query.PayerID
 				LeekWars.post('bank/execute-paypal-payment', {payment_id, paypal_token: token, payer_id}).then(data => {
 					this.$store.commit('update-crystals', data.crystals)
 					this.$router.replace('/bank/validate/success/' + data.crystals + '/PayPal')
 				}).error(error => {
-					this.$router.replace('/bank/validate/failed/PayPal/' + error)
+					this.$router.replace('/bank/validate/failed/PayPal/' + error.error)
+					this.error = true
 				})
 			} else {
 				LeekWars.post('bank/execute-starpass-payment', {code: (window as any).__STARPASS_CODE}).then(data => {
 					this.$store.commit('update-crystals', data.crystals)
 					this.$router.replace('/bank/validate/success/' + data.crystals + '/StarPass')
 				}).error(error => {
-					this.$router.replace('/bank/validate/failed/StarPass/' + error)
+					this.$router.replace('/bank/validate/failed/StarPass/' + error.error)
+					this.error = true
 				})
 			}
 		}
