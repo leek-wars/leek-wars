4445969d51504cc54ec758e43ba6926ad26e7bc8
Pilow <pierre.laupretre@gmail.com>
2019-09-20 21:57:15 +0200

    [popup] Lazy loading of popup content

--- src/component/popup.vue
+++ src/component/popup.vue
@@ -1,24 +1,26 @@
 <template lang="html">
 	<v-dialog :value="value" :max-width="width" :persistent="persistent" @input="$emit('input', $event)">
-		<div class="title">
-			<slot name="title"></slot>
-			<div class="options">
-				<div class="option" @click="close">
-					<i class="material-icons">clear</i>
+		<template v-if="content_created">
+			<div class="title">
+				<slot name="title"></slot>
+				<div class="options">
+					<div class="option" @click="close">
+						<i class="material-icons">clear</i>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div :class="{full: full}" class="content">
-			<slot></slot>
-		</div>
-		<div v-if="!!$slots['actions']" class="actions">
-			<slot name="actions"></slot>
-		</div>
+			<div :class="{full: full}" class="content">
+				<slot></slot>
+			</div>
+			<div v-if="!!$slots['actions']" class="actions">
+				<slot name="actions"></slot>
+			</div>
+		</template>
 	</v-dialog>
 </template>
 
 <script lang="ts">
-	import { Component, Prop, Vue } from 'vue-property-decorator'
+	import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
 	@Component({ name: "popup" })
 	export default class Popup extends Vue {
 		@Prop() value!: boolean
@@ -26,6 +28,14 @@
 		@Prop() width!: number
 		@Prop() full!: boolean
 		@Prop() persistent!: Boolean
+		content_created: boolean = false
+		created() {
+			this.$watch('value', (new_value, old_value) => {
+				if (new_value === true) {
+					this.content_created = true
+				}
+			})
+		}
 		close() {
 			this.$emit('input', false)
 		}
