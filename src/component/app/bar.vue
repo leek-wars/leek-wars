fd19610b8e5f51c8ffe96ebc3da2b414354de1bb
Pilow <pierre.laupretre@gmail.com>
2019-12-02 23:10:35 +0100

    [menu] Remove min-width of menus

--- src/component/app/bar.vue
+++ src/component/app/bar.vue
@@ -18,7 +18,7 @@
 					<span v-show="$store.state.unreadMessages > 0" class="counter messages-counter">{{ $store.state.unreadMessages }}</span>
 				</div>
 				<div v-ripple v-show="LeekWars.menuExpanded || $store.state.unreadNotifications > 0" class="action header-button mobile notifications-button">
-					<v-menu :nudge-bottom="0" :min-width="400" :max-width="400" :max-height="400" bottom offset-y @input="readNotifications">
+					<v-menu :nudge-bottom="0" :max-width="400" :max-height="400" bottom offset-y @input="readNotifications">
 						<div slot="activator" class="header-button notifications-button">
 							<i class="icon material-icons">info</i>
 							<span v-show="$store.state.unreadNotifications > 0" class="counter notifications-counter">{{ $store.state.unreadNotifications }}</span>
