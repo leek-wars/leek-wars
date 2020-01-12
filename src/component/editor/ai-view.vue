2bfe9854a6a4c9905881b80a6e6922545e3befc2
Pilow <pierre.laupretre@gmail.com>
2020-01-12 14:27:20 +0100

    [ai-view] Fix scroll lock

--- src/component/editor/ai-view.vue
+++ src/component/editor/ai-view.vue
@@ -163,7 +163,7 @@
 				const codeMirrorScroll = codeMirrorElement.querySelector('.CodeMirror-scroll') as HTMLElement
 				if (codeMirrorScroll) {
 					codeMirrorScroll.addEventListener('wheel', (e: WheelEvent) => {
-						if (e.deltaY > 0 && Math.abs(codeMirrorScroll.scrollTop - (codeMirrorScroll.scrollHeight - codeMirrorScroll.offsetHeight + 15)) < 1) {
+						if (e.deltaY > 0 && Math.abs(codeMirrorScroll.scrollTop - (codeMirrorScroll.scrollHeight - codeMirrorScroll.offsetHeight + 15)) <= 1) {
 							e.preventDefault()
 						}
 					})
