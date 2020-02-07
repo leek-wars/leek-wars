501f37249349d874feb8589ae59942a94becf3cf
Pilow <pierre.laupretre@gmail.com>
2020-02-07 22:58:30 +0100

    [effect] Implement new modifiers in the market

--- src/component/market/effect.vue
+++ src/component/market/effect.vue
@@ -5,11 +5,23 @@
 		</i18n>
 		<span v-else-if="effect.value2 == 0" v-html="$t('effect.type_' + effect.id + '_fixed', [format(effect.value1)])"></span>
 		<span v-else v-html="$t('effect.type_' + effect.id, [effect.value1, format(effect.value1 + effect.value2)])"></span>
-		<b v-if="effect.modifiers & 1">&nbsp;{{ $t('effect.multiplied_target') }}</b>
+		<span v-if="effect.modifiers & EffectModifier.ON_CASTER">
+			<template v-if="effectThe">
+				{{ $t('effect.the_caster') }}
+			</template>
+			<template v-else>
+				{{ $t('effect.to_the_caster') }}
+			</template>
+		</span>
+		<b v-if="effect.modifiers & EffectModifier.MULTIPLIED_BY_TARGETS">&nbsp;{{ $t('effect.multiplied_target') }}</b>
 		
-		<i18n v-if="effect.turns > 0" path="effect.on_n_turns">
+		<b v-if="effect.turns === -1">{{ $t('effect.infinite') }}</b>
+		<i18n v-else-if="effect.turns > 0" path="effect.on_n_turns">
 			<span slot="turns" v-html="$tc('effect.n_turns', effect.turns)"></span>
 		</i18n>
+		<span v-if="effect.modifiers & EffectModifier.STACKABLE">
+			(<b>{{ $t('effect.stackable') }}</b>)
+		</span>
 
 		<tooltip v-if="enemies && !allies">
 			<template v-slot:activator="{ on }">
@@ -45,28 +57,24 @@
 			</template>
 			<span>{{ $t('effect.target_not_summons') }}</span>
 		</tooltip>
-
-		<tooltip v-if="always_caster">
-			<template v-slot:activator="{ on }">
-				<span class="always-caster" v-on="on"></span>
-			</template>
-			<span>{{ $t('effect.target_always_caster') }}</span>
-		</tooltip>
 	</div>
 </template>
 
 <script lang="ts">
-	import { Effect } from '@/model/effect'
+	import { Effect, EffectModifier, EffectType } from '@/model/effect'
 	import { Component, Prop, Vue } from 'vue-property-decorator'
 	@Component({ name: 'effect-view' })
 	export default class EffectView extends Vue {
 		@Prop() effect!: Effect
+		EffectModifier = EffectModifier
 		get enemies() { return this.effect.targets & 1 }
 		get allies(): boolean { return (this.effect.targets & (1 << 1)) !== 0 }
 		get caster(): boolean { return (this.effect.targets & (1 << 2)) !== 0 }
 		get nonSummons(): boolean { return (this.effect.targets & (1 << 3)) !== 0 }
 		get summons(): boolean { return (this.effect.targets & (1 << 4)) !== 0 }
-		get always_caster(): boolean { return (this.effect.targets & (1 << 5)) !== 0 }
+		get effectThe(): boolean {
+			return this.effect.id === EffectType.HEAL
+		}
 		format(n: number) {
 			if (Math.floor(n) !== n) {
 				return n.toFixed(2)
