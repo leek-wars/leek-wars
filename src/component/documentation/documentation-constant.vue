f2ac242c9f4c26530630bfd854c5a20633db6807
Pilow <pierre.laupretre@gmail.com>
2020-02-15 10:38:36 +0100

    [doc] Add item preview for constant content

--- src/component/documentation/documentation-constant.vue
+++ src/component/documentation/documentation-constant.vue
@@ -2,7 +2,9 @@
 	<div v-if="constant">
 		<h2 class="searchable">{{ constant.name }}</h2>
 		<div v-if="constant.deprecated" class="deprecated-message">Cette constante est dépréciée.</div>
-		<div class="content searchable" v-html="$t('documentation.const_' + constant.name)"></div>
+		<chip-preview v-if="constant.name.startsWith('CHIP_')" :chip="LeekWars.chips[constant.value]" />
+		<weapon-preview v-else-if="constant.name.startsWith('WEAPON_')" :weapon="LeekWars.weapons[constant.value]" />
+		<div v-else class="content searchable" v-html="$t('documentation.const_' + constant.name)"></div>
 		<h4>{{ $t('documentation.value') }}</h4>
 		<ul>
 			<li>{{ constant.name }} = {{ constant.value }}</li>
@@ -11,9 +13,11 @@
 </template>
 
 <script lang="ts">
+	import ChipPreview from '@/component/market/chip-preview.vue'
+	import WeaponPreview from '@/component/market/weapon-preview.vue'
 	import { Constant } from '@/model/constant'
 	import { Component, Prop, Vue } from 'vue-property-decorator'
-	@Component({ name: 'documentation-constant' })
+	@Component({ name: 'documentation-constant', components: { WeaponPreview, ChipPreview }})
 	export default class DocumentationConstant extends Vue {
 		@Prop() constant!: Constant
 	}
