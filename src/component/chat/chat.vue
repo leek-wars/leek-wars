2797a84c594901dab24a786b8fa129e36d52499a
Pilow <pierre.laupretre@gmail.com>
2020-03-29 13:20:53 +0200

    [chat] Lighter shadows on chat bubbles

--- src/component/chat/chat.vue
+++ src/component/chat/chat.vue
@@ -302,7 +302,7 @@
 		border-radius: 4px;
 		background: white;
 		position: relative;
-		box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12);
+		box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.1), 0px 1px 1px 0px rgba(0,0,0,0.07), 0px 1px 3px 0px rgba(0,0,0,0.06);
 		min-width: 0;
 		.report, .mute, .unmute {
 			display: none;
