6b21b532976e9bfb0bcd76649b68d303c4376184
Pilow <pierre.laupretre@gmail.com>
2020-07-25 14:06:31 +0200

    [service-worker] Enable everytime

--- src/model/serviceworker.ts
+++ src/model/serviceworker.ts
@@ -1,25 +1,23 @@
 import { LeekWars } from '@/model/leekwars'
 import { register } from 'register-service-worker'
 
-if (process.env.NODE_ENV === 'production') {
-	register(`${process.env.BASE_URL}service-worker.js`, {
-		registered(registration) {
-			LeekWars.service_worker = registration
-		},
-		ready(registration) {
-			LeekWars.service_worker = registration
-		},
-		cached(registration) {
-			LeekWars.service_worker = registration
-		},
-		updated() {
-			// updated
-		},
-		offline() {
-			// offline
-		},
-		error(error) {
-			console.error("Service worker error", error)
-		},
-	})
-}
+register(`${process.env.BASE_URL}service-worker.js`, {
+	registered(registration) {
+		LeekWars.service_worker = registration
+	},
+	ready(registration) {
+		LeekWars.service_worker = registration
+	},
+	cached(registration) {
+		LeekWars.service_worker = registration
+	},
+	updated() {
+		// updated
+	},
+	offline() {
+		// offline
+	},
+	error(error) {
+		console.error("Service worker error", error)
+	},
+})
