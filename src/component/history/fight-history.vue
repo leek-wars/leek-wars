fd43b8224bde1de63ebd7a31f765aaf3c71bc6c7
Pilow <pierre.laupretre@gmail.com>
2020-09-05 23:18:55 +0200

    [history] Improve fight history style

--- src/component/history/fight-history.vue
+++ src/component/history/fight-history.vue
@@ -1,48 +1,49 @@
 <template lang="html">
 	<div :class="{generating: fight.status == 0, win: fight.result == 'win', defeat: fight.result == 'defeat', draw: fight.result == 'draw'}" class="fight">
 		<div v-if="fight.type == FightType.BATTLE_ROYALE" class="fighters">
+			<div class="fighter left"><div>Battle</div></div>
 			<div class="center">
 				<router-link :to="'/fight/' + fight.id">
-					<img v-if="fight.status == 0" src="/image/icon/gearing.png">
-					<img v-else src="/image/icon/garden.png">
+					<v-icon v-if="fight.status == 0" class="timersand">mdi-timer-sand-empty</v-icon>
+					<v-icon v-else>mdi-sword-cross</v-icon>
 				</router-link>
 			</div>
-			<div class="fighter">Battle royale</div>
+			<div class="fighter right"><div>Royale</div></div>
 		</div>
 		<div v-else class="fighters">
-			<router-link v-if="fight.type == FightType.SOLO" :to="'/leek/' + fight.leeks1[0].id">
+			<router-link v-if="fight.type == FightType.SOLO" :to="'/leek/' + fight.leeks1[0].id" class="fighter">
 				<rich-tooltip-leek :id="fight.leeks1[0].id" v-slot="{ on }">
-					<div class="fighter" v-on="on">{{ fight.leeks1[0].name }}</div>
+					<div v-on="on">{{ fight.leeks1[0].name }}</div>
 				</rich-tooltip-leek>
 			</router-link>
-			<router-link v-else-if="fight.type == FightType.FARMER" :to="'/farmer/' + fight.farmer1">
+			<router-link v-else-if="fight.type == FightType.FARMER" :to="'/farmer/' + fight.farmer1" class="fighter">
 				<rich-tooltip-farmer :id="fight.farmer1" v-slot="{ on }">
-					<div class="fighter" v-on="on">({{ fight.farmer1_name }})</div>
+					<div v-on="on">({{ fight.farmer1_name }})</div>
 				</rich-tooltip-farmer>
 			</router-link>
-			<router-link v-else-if="fight.type == FightType.TEAM" :to="'/team/' + fight.team1">
-				<div class="fighter">[{{ fight.team1_name }}]</div>
+			<router-link v-else-if="fight.type == FightType.TEAM" :to="'/team/' + fight.team1" class="fighter">
+				<div>[{{ fight.team1_name }}]</div>
 			</router-link>
 			<div class="center">
 				<router-link :to="'/fight/' + fight.id">
-					<v-icon v-if="fight.status == 0">mdi-timer-sand-empty</v-icon>
-					<img v-else-if="fight.context == FightContext.CHALLENGE" src="/image/icon/flag.png">
-					<img v-else-if="fight.context == FightContext.TOURNAMENT" src="/image/icon/trophy.png">
-					<img v-else src="/image/icon/garden.png">
+					<v-icon v-if="fight.status == 0" class="timersand">mdi-timer-sand-empty</v-icon>
+					<v-icon v-else-if="fight.context == FightContext.CHALLENGE">mdi-flag-outline</v-icon>
+					<v-icon v-else-if="fight.context == FightContext.TOURNAMENT">mdi-trophy-outline</v-icon>
+					<img v-else src="/image/icon/black/garden.png">
 				</router-link>
 			</div>
-			<router-link v-if="fight.type == FightType.SOLO" :to="'/leek/' + fight.leeks2[0].id">
+			<router-link v-if="fight.type == FightType.SOLO" :to="'/leek/' + fight.leeks2[0].id" class="fighter">
 				<rich-tooltip-leek :id="fight.leeks2[0].id" v-slot="{ on }">
-					<div class="fighter" v-on="on">{{ fight.leeks2[0].name }}</div>
+					<div v-on="on">{{ fight.leeks2[0].name }}</div>
 				</rich-tooltip-leek>
 			</router-link>
-			<router-link v-else-if="fight.type == FightType.FARMER" :to="'/farmer/' + fight.farmer2">
+			<router-link v-else-if="fight.type == FightType.FARMER" :to="'/farmer/' + fight.farmer2" class="fighter">
 				<rich-tooltip-farmer :id="fight.farmer2" v-slot="{ on }">
-					<div class="fighter" v-on="on">({{ fight.farmer2_name }})</div>
+					<div v-on="on">({{ fight.farmer2_name }})</div>
 				</rich-tooltip-farmer>
 			</router-link>
-			<router-link v-else-if="fight.type == FightType.TEAM" :to="'/team/' + fight.team2">
-				<div class="fighter">[{{ fight.team2_name }}]</div>
+			<router-link v-else-if="fight.type == FightType.TEAM" :to="'/team/' + fight.team2" class="fighter">
+				<div>[{{ fight.team2_name }}]</div>
 			</router-link>
 		</div>
 		<div class="time">{{ LeekWars.formatDuration(fight.date) }}</div>
@@ -70,46 +71,57 @@
 		height: 42px;
 		white-space: nowrap;
 		background: white;
+		position: relative;
 		.center {
-			display: inline-block;
-			background: #555;
-			width: 36px;
+			background: rgba(255, 255, 255, 0.25);
+			&:hover {
+				background: rgba(255, 255, 255, 0.75);
+			}
+			flex: 36px 0 0;
 			height: 42px;
-			margin-left: -4px;
 			img {
 				width: 22px;
 				height: 22px;
 				margin: 10px 6px;
+				opacity: 0.8;
 			}
 			i {
-				color: white;
+				color: #444;
 				line-height: 42px;
-				animation: rotate 2s linear infinite;
+				&.timersand {
+					animation: rotate 2s linear infinite;
+				}
 			}
 		}
 		.fighters {
 			height: 42px;
-			margin: 0 auto;
+			display: flex;
 		}
 		.fighter {
-			padding: 4px 6px;
-			width: 109px;
-			vertical-align: top;
-			display: inline-block;
-			overflow: hidden;
-			text-overflow: ellipsis;
+			padding: 4px 0;
 			height: 30px;
 			line-height: 30px;
-		}
-		.fighter:last-child {
-			margin-left: -4px;
+			flex: 1;
+			overflow: hidden;
+			&.left {
+				text-align: right;
+			}
+			&.right {
+				text-align: left;
+			}
+			div {
+				padding: 0 6px;
+				overflow: hidden;
+				text-overflow: ellipsis;
+			}
 		}
 		.time {
-			color: #888;
+			position: absolute;
+			bottom: 2px;
+			right: 8px;
+			color: #777;
 			font-size: 11px;
-			margin-right: 5px;
 			text-align: right;
-			margin-top: -15px;
 		}
 	}
 	.win {
