f0ea10af4ec25fb59bffc1fff19652d60839c79e
Pilow <pierre.laupretre@gmail.com>
2020-11-25 22:32:44 +0100

    [chat] Add days separators

--- src/model/chat.ts
+++ src/model/chat.ts
@@ -10,6 +10,7 @@ class ChatMessage {
 	author!: Farmer
 	texts: string[] = []
 	time!: number
+	day!: number
 }
 
 class ChatWindow {
@@ -33,22 +34,47 @@ class Chat {
 	}
 	add(authorID: number, authorName: string, authorAvatarChanged: number, authorGrade: string, messageRaw: string, time: number) {
 		const message = this.formatMessage(messageRaw, authorName)
+		const day = new Date(time * 1000).getDate()
+		let separator = false
 		if (this.messages.length) {
 			const lastMessage = this.messages[this.messages.length - 1]
 			if (lastMessage.author.id === authorID && time - lastMessage.time < 120) {
 				lastMessage.texts.push(message)
 				return
 			}
+			if (lastMessage.day !== day) {
+				separator = true
+			}
+		} else {
+			separator = true
+		}
+		// Separator
+		if (separator) {
+			this.messages.push({
+				author: { id: -1, name: '', avatar_changed: 0, grade: '' },
+				texts: [''],
+				time,
+				day
+			} as ChatMessage)
 		}
 		this.messages.push({
 			author: { id: authorID, name: authorName, avatar_changed: authorAvatarChanged, grade: authorGrade },
 			texts: [message],
-			time
+			time,
+			day
 		} as ChatMessage)
 	}
 
 	set_messages(messages: any[]) {
 		const prepared_messages: any[] = []
+		if (messages.length) {
+			prepared_messages.push({
+				author: { id: -1, name: '', avatar_changed: 0, grade: '' },
+				texts: [''],
+				time: messages[0][4],
+				day: new Date(messages[0][4] * 1000).getDate()
+			})
+		}
 		for (const raw_message of messages) {
 			const message = this.formatMessage(raw_message[3], raw_message[2])
 			if (prepared_messages.length) {
@@ -61,8 +87,9 @@ class Chat {
 			prepared_messages.push({
 				author: { id: raw_message[1], name: raw_message[2], avatar_changed: raw_message[6], grade: raw_message[5] },
 				texts: [message],
-				time: raw_message[4]
-			} as ChatMessage)
+				time: raw_message[4],
+				day: new Date(raw_message[4] * 1000).getDate()
+			})
 		}
 		this.messages = prepared_messages
 	}
@@ -71,7 +98,8 @@ class Chat {
 		this.messages.push({
 			author: { id: 0, name: "Leek Wars" } as Farmer,
 			texts: [i18n.t('main.br_started_message') as string, '' + fightID],
-			time
+			time,
+			day: new Date(time * 1000).getDate()
 		})
 	}
 
