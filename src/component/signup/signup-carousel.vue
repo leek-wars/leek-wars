3af0156dbcf7ae408af07ef9a6e1623fdef6bf54
Pilow <pierre.laupretre@gmail.com>
2021-03-05 21:31:49 +0100

    [signup] Optimize carousel

--- src/component/signup/signup-carousel.vue
+++ src/component/signup/signup-carousel.vue
@@ -1,12 +1,11 @@
 <template lang="html">
 	<div>
 		<swiper :options="swiperOption" @click-slide="handleClickSlide">
-			<swiper-slide v-for="(image, i) of images" :key="i" class="slide">
-				<img :src="'/image/' + image[0]">
-				<div class="legend">{{ $t(image[1]) }}</div>
+			<swiper-slide v-for="image of images" :key="image.id" class="slide">
+				<img :src="'/image/' + image.image">
+				<div class="legend">{{ image.legend_tr }}</div>
 			</swiper-slide>
 		</swiper>
-
 		<div v-if="bigImage" class="bigscreen" @click="bigImage = null">
 			<img :src="'/image/' + bigImage">
 			<div class="biglegend">{{ $t(bigImageLegend) }}</div>
@@ -15,10 +14,9 @@
 </template>
 
 <script lang="ts">
-	import ChangelogVersion from '@/component/changelog/changelog-version.vue'
 	import { i18n, mixins } from '@/model/i18n'
 	import { LeekWars } from '@/model/leekwars'
-	import { Component, Vue } from 'vue-property-decorator'
+	import { Component, Vue, Watch } from 'vue-property-decorator'
 
 	import 'swiper/css/swiper.css'
 	import VueAwesomeSwiper from 'vue-awesome-swiper'
@@ -30,22 +28,22 @@
 		bigImage: string | null = null
 		bigImageLegend: string = ''
 		images = [
-			["signup/new/fight_factory_small.jpg", "fight_factory"],
-			["signup/new/leek_small.jpg", "leek_page"],
-			["signup/new/br_small.jpg", "fight_ice"],
-			["signup/new/editor_small.jpg", "editor"],
-			["signup/new/forest.jpg", "fight_forest"],
-			["signup/new/market_small.jpg", "market"],
-			["signup/new/desert.jpg", "fight_desert"],
-			["signup/new/ranking_small.jpg", "ranking"],
-			["signup/new/trophies_small.jpg", "trophies"],
-			["signup/new/tournament_small.jpg", "tournament"],
-			["app/preview1.jpg", "android_app"],
-			["app/preview3.jpg", "android_app"],
-			["app/preview5.jpg", "android_app"],
-			["app/preview6.jpg", "android_app"],
-			["app/preview7.jpg", "android_app"],
-			["signup/new/mona.jpg", "pixel_art"],
+			{id: 1, image: "signup/new/fight_factory_small.jpg", legend: "fight_factory", legend_tr: ''},
+			{id: 2, image: "signup/new/leek_small.jpg", legend: "leek_page", legend_tr: ''},
+			{id: 3, image: "signup/new/br_small.jpg", legend: "fight_ice", legend_tr: ''},
+			{id: 4, image: "signup/new/editor_small.jpg", legend: "editor", legend_tr: ''},
+			{id: 5, image: "signup/new/forest.jpg", legend: "fight_forest", legend_tr: ''},
+			{id: 6, image: "signup/new/market_small.jpg", legend: "market", legend_tr: ''},
+			{id: 7, image: "signup/new/desert.jpg", legend: "fight_desert", legend_tr: ''},
+			{id: 8, image: "signup/new/ranking_small.jpg", legend: "ranking", legend_tr: ''},
+			{id: 9, image: "signup/new/trophies_small.jpg", legend: "trophies", legend_tr: ''},
+			{id: 10, image: "signup/new/tournament_small.jpg", legend: "tournament", legend_tr: ''},
+			{id: 11, image: "app/preview1.jpg", legend: "android_app", legend_tr: ''},
+			{id: 12, image: "app/preview3.jpg", legend: "android_app", legend_tr: ''},
+			{id: 13, image: "app/preview5.jpg", legend: "android_app", legend_tr: ''},
+			{id: 14, image: "app/preview6.jpg", legend: "android_app", legend_tr: ''},
+			{id: 15, image: "app/preview7.jpg", legend: "android_app", legend_tr: ''},
+			{id: 16, image: "signup/new/mona.jpg", legend: "pixel_art", legend_tr: ''},
 		]
 		swiperOption = {
 			slidesPerView: 'auto',
@@ -57,11 +55,20 @@
 			}
 		}
 
+		@Watch('$i18n.locale', {immediate: true})
+		updateLocale() {
+			setTimeout(() => {
+				for (const image of this.images) {
+					image.legend_tr = this.$t(image.legend) as string
+				}
+			}, 200)
+		}
+
 		handleClickSlide(_: number, i: number) {
 			if (LeekWars.mobile) { return }
 			const image = this.images[i]
-			this.bigImage = image[0].replace('_small', '')
-			this.bigImageLegend = image[1]
+			this.bigImage = image.image.replace('_small', '')
+			this.bigImageLegend = image.legend
 		}
 	}
 </script>
