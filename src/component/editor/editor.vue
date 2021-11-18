6e656468de22bef728d6cf6b5062eca42c067a43
pseudonym-2669 <55923733+pseudonym-2669@users.noreply.github.com>
2021-11-18 13:12:39 +0100

    [Editor] Remove shortcut13

--- src/component/editor/editor.vue
+++ src/component/editor/editor.vue
@@ -230,7 +230,6 @@
 
 				<ul class="shortcuts">
 					<li v-html="$t('shortcut.shortcut_1')"></li>
-					<!-- <li v-html="$t('shortcut.shortcut_13')"></li> -->
 					<li v-html="$t('shortcut.shortcut_2')"></li>
 					<li v-html="$t('shortcut.shortcut_3')"></li>
 					<li v-html="$t('shortcut.shortcut_4')"></li>
