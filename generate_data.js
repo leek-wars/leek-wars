bffeefe138cec2617d43cf2b6821597019ce3af7
Pilow <pierre.laupretre@gmail.com>
2022-04-24 17:53:47 +0200

    [weapon] New lightninger

--- generate_data.js
+++ generate_data.js
@@ -35,7 +35,7 @@ for (const value of values) {
 		}).catch((err) => {
 			console.log("ERROR request failed for", value[0], ":", err.statusCode, err.error)
 			process.exit()
-	})) }, r += 200)
+	})) }, r += 0)
 }
 
 setTimeout(() => {
