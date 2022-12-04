7c6d4ad32d99c2cb9f956354441dacde606bc3dd
Pilow <pierre.laupretre@gmail.com>
2022-12-04 23:31:16 +0100

    [tournament] Fix skins

--- src/component/tournament/tournament-block.vue
+++ src/component/tournament/tournament-block.vue
@@ -1,7 +1,7 @@
 <template>
 	<a :xlink:href="link" @click="click">
 		<rect :width="size" :height="size" :x="x" :y="y" :me="item ? item.me : false" :class="{'no-fight': !item}" class="entry" />
-		<leek-image v-if="item && item.data" :x="x + 1" :y="y + 1" :width="size - 2" :height="size - 2" :leek="{level: item.data[0], skin: item.data[1], hat: item.data[2], weapon: item.data[3]}" :scale="1" :invert="invert" @mouseenter.native="mouseenter" @mouseleave.native="mouseleave" />
+		<leek-image v-if="item && item.data" :x="x + 1" :y="y + 1" :width="size - 2" :height="size - 2" :leek="{level: item.data[0], skin: item.data[1], hat: item.data[2], weapon: item.data[3], metal: item.data[4], face: item.data[5]}" :scale="1" :invert="invert" @mouseenter.native="mouseenter" @mouseleave.native="mouseleave" />
 		<image v-else :win="item ? item.win : false" :width="size - 2" :height="size - 2" :x="x + 1" :y="y + 1" :xlink:href="image" @mouseenter="mouseenter" @mouseleave="mouseleave" />
 	</a>
 </template>
