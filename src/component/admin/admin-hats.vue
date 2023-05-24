c82190c950a70ca15d9159fd76ec4943b932b418
Pilow <pierre.laupretre@gmail.com>
2023-05-24 14:31:33 +0200

    [admin] hats

--- src/component/admin/admin-hats.vue
+++ src/component/admin/admin-hats.vue
@@ -4,7 +4,7 @@
 			<h1><router-link to="/admin">Administration</router-link> > Chapeaux</h1>
 		</div>
 		<panel v-for="hat in LeekWars.hats" :key="hat.id">
-			<h4>{{ $t('hat.' + hat.name) }}</h4>
+			<h4>{{ $t('hat.' + hat.name) }} ({{ hat.id }})</h4>
 			<div class="leeks">
 				<leek-image v-for="level in [1, 10, 25, 50, 100, 200, 301]" :key="level" :leek="{level, hat: hat.id, weapon: random_weapon(), skin: random_skin()}" :scale="0.8" />
 			</div>
