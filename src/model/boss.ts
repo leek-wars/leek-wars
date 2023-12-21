d339481f0810d7998714f1ee8ae6921179381fde
Pilow <pierre.laupretre@gmail.com>
2023-12-21 15:58:10 +0100

    update boss levels

--- src/model/boss.ts
+++ src/model/boss.ts
@@ -11,6 +11,6 @@ export class Boss {
 
 export const BOSSES = {
 	1: { id: 1, name: "nasu_samurai", level: 50, weapon: 408, hat: null, type: 'boss', scale: 0.85, difficulty: 1 },
-	2: { id: 2, name: "fennel_king", level: 100, weapon: 409, hat: 5, type: 'boss', scale: 0.95, difficulty: 2 },
-	3: { id: 3, name: "evil_pumpkin", level: 200, weapon: 410, hat: null, type: 'boss', scale: 0.8, difficulty: 3 },
+	2: { id: 2, name: "fennel_king", level: 200, weapon: 409, hat: 5, type: 'boss', scale: 0.95, difficulty: 2 },
+	3: { id: 3, name: "evil_pumpkin", level: 300, weapon: 410, hat: null, type: 'boss', scale: 0.8, difficulty: 3 },
 } as {[key: number]: Boss}
